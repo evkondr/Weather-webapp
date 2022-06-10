3984e598e637a972ba630f6c7084c83e8437c32d
evkondr <kondratiev.evg@gmail.com>
2022-06-10 13:59:41 +0300

    reafactor: apply eslint

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,15 @@
+import React from "react";
+
 function App() {
-  return <div className="wrap">
-      
-  </div>
+  return (
+    <div className="wrap">
+      <div className="weather-widget">
+        <div className="city-search">
+          <input type="text" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
